3eeedcbc01075fa552cb6508c9380033c4c205bb
kalahasv <104052707+kalahasv@users.noreply.github.com>
2023-11-17 11:01:36 -0800

    changed structure to convert to csv

--- main.py
+++ main.py
@@ -1,41 +1,22 @@
 import PyPDF2 as pf
 from pathlib import Path
+import pdftotree
+from bs4 import BeautifulSoup
+import pandas as pd
+from IPython.display import display
+import tabula
 
 
-def convert_to_job(text):
-    text_arr = text.splitlines()
-    #how to know what belongs to what? can use position for the  first 6 if I split lines
-    #what after that? for certificates and supplier qualifications i can look for keywords and use a boolean
-    #it looks like tolerance is always there: look for text following "tolerance:". May or may not have locations after it. 
-    # Seems like it's always two lines so i can just grab the next line
-    info_line_1 = text_arr[2] #contains job id, date, and contact information
-    #regex match to J_ID format
-
-
-
-    print(text_arr)
-
-
-
-
-def get_line(pdfFile):
-    with open(pdfFile) as f:
-        reader = pf.PdfReader(pdfFile)
-
-        print(len(reader.pages))
-
-        page = reader.pages[0]
-
-        text = page.extract_text()
-        text2 = page.get_contents()
-        print(text)
-        print("Conversion")
-        convert_to_job(text)
-
-
+def pdf_to_df(file_name):
+    df = tabula.read_pdf(file_name, pages='all')
+    
+    for i in range(len(df)):
+        print("####",i)
+        print(df[i])
 
 if __name__ == '__main__':
 
-    file_name = '057531C-traveler.pdf'
+    #file_name = '057531C-traveler.pdf'
+    file_name = '05695AD-traveler.pdf'
     path =  Path('jfiles',file_name)
-    get_line(path)
+    pdf_to_df(path)
