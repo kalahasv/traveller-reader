c18450e1e5f7731790e1da2a375c6d2fdf969c58
kalahasv <104052707+kalahasv@users.noreply.github.com>
2023-11-16 16:32:16 -0800

    added a job class and pdf reader functions

---
+++ Job.py
@@ -0,0 +1,14 @@
+class Job:
+
+    def __init__(self,name,po,dd,contact,pid,pr,q):
+        self.name = name
+        self.po = po
+        self.dd = dd
+        self.contact = contact
+        self.pid = pid
+        self.pr = pr
+        self.quantity = q
+    
+
+
+    
--- main.py
+++ main.py
@@ -1,10 +1,41 @@
-from PyPDF2 import PdfReader
+import PyPDF2 as pf
+from pathlib import Path
 
-reader = PdfReader('test document.pdf')
 
-print(len(reader.pages))
+def convert_to_job(text):
+    text_arr = text.splitlines()
+    #how to know what belongs to what? can use position for the  first 6 if I split lines
+    #what after that? for certificates and supplier qualifications i can look for keywords and use a boolean
+    #it looks like tolerance is always there: look for text following "tolerance:". May or may not have locations after it. 
+    # Seems like it's always two lines so i can just grab the next line
+    info_line_1 = text_arr[2] #contains job id, date, and contact information
+    #regex match to J_ID format
 
-page = reader.pages[0]
 
-text = page.extract_text()
-print(text)
+
+    print(text_arr)
+
+
+
+
+def get_line(pdfFile):
+    with open(pdfFile) as f:
+        reader = pf.PdfReader(pdfFile)
+
+        print(len(reader.pages))
+
+        page = reader.pages[0]
+
+        text = page.extract_text()
+        text2 = page.get_contents()
+        print(text)
+        print("Conversion")
+        convert_to_job(text)
+
+
+
+if __name__ == '__main__':
+
+    file_name = '057531C-traveler.pdf'
+    path =  Path('jfiles',file_name)
+    get_line(path)
